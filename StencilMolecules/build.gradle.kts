b3b69d21971f1602e151f938d8014f5c618b3696
HARSH <theharsh1210@gmail.com>
2024-07-05 15:02:06 +0530

    for stencil mobile

--- StencilMolecules/build.gradle.kts
+++ StencilMolecules/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish") // If publishing to Maven repository
 }
 
 android {
@@ -41,10 +42,24 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation ("androidx.compose.ui:ui:1.1.0")
-    implementation ("androidx.compose.material:material:1.1.0")
-    implementation ("androidx.compose.ui:ui-tooling:1.1.0")
-    implementation ("androidx.activity:activity-compose:1.4.0")
-    implementation ("androidx.compose.foundation:foundation:1.1.0")
+    implementation("androidx.compose.ui:ui:1.1.0")
+    implementation("androidx.compose.material:material:1.1.0")
+    implementation("androidx.compose.ui:ui-tooling:1.1.0")
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.compose.foundation:foundation:1.1.0")
+    // Add any other dependencies specific to your library
+}
 
+publishing{
+    publications{
+        register<MavenPublication>("release"){
+            groupId = "com.github.TheNPDev"
+            artifactId = "Chatwave"
+            version = "1.0.0"
+
+            afterEvaluate{
+                from(components["release"])
+            }
+            }
+    }
 }
